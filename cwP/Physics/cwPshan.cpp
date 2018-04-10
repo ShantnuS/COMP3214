738a937512ce81fec94c217a5103d3d0aa2c9bee
ShantnuS <shantnu@outlook.com>
2018-04-11 00:13:06 +0100

    started own work

---
+++ cwP/Physics/cwPshan.cpp
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#include "World.h"
+
+const double PI = 3.141592653589793;
+const double PIo2 = PI / 2.;
+const double PIo4 = PI / 4.;
+const double PI2 = PI * 2.;
+const float lod = PI / 32.;
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "VertexBufferObject.h"
+#include "glslprogram.h"
+#include "stb_image.h"
+#include "utils.h"
+#include <btBulletDynamicsCommon.h>
+
+int main(){
+	return 0;
+}
