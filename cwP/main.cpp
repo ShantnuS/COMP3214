4df4581fb0dd18aa3c50dcefd225a9ad7d0becd0
ShantnuS <shantnu@outlook.com>
2018-04-12 19:27:03 +0100

    added openGL basics

--- cwP/main.cpp
+++ cwP/main.cpp
@@ -1,4 +1,9 @@
- #include <stdio.h>
+//Coursework P - Shantnu Singh 
+
+//Basics 
+#include <stdio.h>
+#include <fstream>
+#include <sstream>
 
 // OpenGL (GLFW/GLEW)
 #include <GL/glew.h>
@@ -23,9 +28,274 @@
 //World header file
 #include "World.h"
 
+GLuint program;
+
+struct Object {
+	GLuint vao;
+	GLuint vert_b;
+	int size;
+	glm::mat4 model;
+	GLuint texID;
+};
+
+struct Normal {
+	glm::vec3 normal;
+	glm::vec3 direction;
+	glm::vec2 uv;
+};
+
+GLfloat cube_VB[] = {
+	-1.0f,-1.0f,-1.0f, // triangle 1 : begin
+	-1.0f,-1.0f, 1.0f,
+	-1.0f, 1.0f, 1.0f, // triangle 1 : end
+	1.0f, 1.0f,-1.0f, // triangle 2 : begin
+	-1.0f,-1.0f,-1.0f,
+	-1.0f, 1.0f,-1.0f, // triangle 2 : end
+	1.0f,-1.0f, 1.0f,
+	-1.0f,-1.0f,-1.0f,
+	1.0f,-1.0f,-1.0f,
+	1.0f, 1.0f,-1.0f,
+	1.0f,-1.0f,-1.0f,
+	-1.0f,-1.0f,-1.0f,
+	-1.0f,-1.0f,-1.0f,
+	-1.0f, 1.0f, 1.0f,
+	-1.0f, 1.0f,-1.0f,
+	1.0f,-1.0f, 1.0f,
+	-1.0f,-1.0f, 1.0f,
+	-1.0f,-1.0f,-1.0f,
+	-1.0f, 1.0f, 1.0f,
+	-1.0f,-1.0f, 1.0f,
+	1.0f,-1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
+	1.0f,-1.0f,-1.0f,
+	1.0f, 1.0f,-1.0f,
+	1.0f,-1.0f,-1.0f,
+	1.0f, 1.0f, 1.0f,
+	1.0f,-1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f,-1.0f,
+	-1.0f, 1.0f,-1.0f,
+	1.0f, 1.0f, 1.0f,
+	-1.0f, 1.0f,-1.0f,
+	-1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
+	-1.0f, 1.0f, 1.0f,
+	1.0f,-1.0f, 1.0f
+};
+
+static void error_callback(int error, const char* description) {
+	fputs(description, stderr);
+	_fgetchar();
+}
+
+GLuint LoadShader(const char * vertex_file_path, const char * fragment_file_path) {
+
+	// Create the shaders
+	GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+	GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+	// Read the Vertex Shader code from the file
+	std::string VertexShaderCode;
+	std::ifstream VertexShaderStream(vertex_file_path, std::ios::in);
+	if (VertexShaderStream.is_open()) {
+		std::string Line = "";
+		while (getline(VertexShaderStream, Line))
+			VertexShaderCode += "\n" + Line;
+		VertexShaderStream.close();
+	}
+	else {
+		printf("Impossible to open %s. Are you in the right directory ? Don't forget to read the FAQ !\n", vertex_file_path);
+		getchar();
+		return 0;
+	}
+
+	// Read the Fragment Shader code from the file
+	std::string FragmentShaderCode;
+	std::ifstream FragmentShaderStream(fragment_file_path, std::ios::in);
+	if (FragmentShaderStream.is_open()) {
+		std::string Line = "";
+		while (getline(FragmentShaderStream, Line))
+			FragmentShaderCode += "\n" + Line;
+		FragmentShaderStream.close();
+	}
+
+	GLint Result = GL_FALSE;
+	int InfoLogLength;
+
+
+	// Compile Vertex Shader
+	printf("Compiling shader : %s\n", vertex_file_path);
+	char const * VertexSourcePointer = VertexShaderCode.c_str();
+	glShaderSource(VertexShaderID, 1, &VertexSourcePointer, NULL);
+	glCompileShader(VertexShaderID);
+
+	// Check Vertex Shader
+	glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
+	glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+	if (InfoLogLength > 0) {
+		std::vector<char> VertexShaderErrorMessage(InfoLogLength + 1);
+		glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL, &VertexShaderErrorMessage[0]);
+		printf("%s\n", &VertexShaderErrorMessage[0]);
+	}
+
+
+
+	// Compile Fragment Shader
+	printf("Compiling shader : %s\n", fragment_file_path);
+	char const * FragmentSourcePointer = FragmentShaderCode.c_str();
+	glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer, NULL);
+	glCompileShader(FragmentShaderID);
+
+	// Check Fragment Shader
+	glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
+	glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+	if (InfoLogLength > 0) {
+		std::vector<char> FragmentShaderErrorMessage(InfoLogLength + 1);
+		glGetShaderInfoLog(FragmentShaderID, InfoLogLength, NULL, &FragmentShaderErrorMessage[0]);
+		printf("%s\n", &FragmentShaderErrorMessage[0]);
+	}
+
+
+
+	// Link the program
+	printf("Linking program\n");
+	GLuint ProgramID = glCreateProgram();
+	glAttachShader(ProgramID, VertexShaderID);
+	glAttachShader(ProgramID, FragmentShaderID);
+	glLinkProgram(ProgramID);
+
+	// Check the program
+	glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
+	glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+	if (InfoLogLength > 0) {
+		std::vector<char> ProgramErrorMessage(InfoLogLength + 1);
+		glGetProgramInfoLog(ProgramID, InfoLogLength, NULL, &ProgramErrorMessage[0]);
+		printf("%s\n", &ProgramErrorMessage[0]);
+	}
+
+
+	glDetachShader(ProgramID, VertexShaderID);
+	glDetachShader(ProgramID, FragmentShaderID);
+
+	glDeleteShader(VertexShaderID);
+	glDeleteShader(FragmentShaderID);
+
+	return ProgramID;
+}
+
+glm::vec2 getPolar(glm::vec3 v){
+	float r = sqrt(v.x*v.x + v.y*v.y + v.z*v.z);
+	return glm::vec2(atan(v.y / v.x), acos(v.z / r));
+}
+
+glm::vec3 getPolarCoordinate(float i, float j) {
+	float x = cos(i)*sin(j);
+	float y = sin(i)*sin(j);
+	float z = cos(j);
+
+	return glm::vec3(x, y, z);
+}
+
+std::vector<Normal> getNormal(std::vector<glm::vec3> v) {
+	std::vector<glm::vec3> tempVec;
+	std::vector<Normal> returnThing;
+
+	for (int i = 0; i < v.size(); i += 3) {
+		tempVec.push_back(normalize(cross(v[i + 1] - v[i], v[i + 2] - v[i])));
+		tempVec.push_back(normalize(cross(v[i + 1] - v[i], v[i + 2] - v[i])));
+		tempVec.push_back(normalize(cross(v[i + 1] - v[i], v[i + 2] - v[i])));
+	}
+
+	std::vector<glm::vec2> texCoord;
+	for (int i = 0; i < v.size(); i++) {
+		texCoord.push_back(getPolar(v[i]));
+	}
+
+	for (int i = 0; i < v.size(); i++) {
+		Normal n;
+		n.direction = v[i];
+		n.normal = tempVec[i];
+		n.uv = texCoord[i];
+		returnThing.push_back(n);
+	}
+
+	return returnThing;
+}
+
+std::vector<Normal> generateCube(){
+	std::vector<glm::vec3> v;
+	for (int i = 0; i < 36; i++)
+		v.push_back(glm::vec3(cube_VB[i * 3], cube_VB[i * 3 + 1], cube_VB[i * 3 + 2]));
+
+	return getNormal(v);
+}
+
+std::vector<Normal> generateSphere(float step) {
+
+	std::vector<glm::vec3> sphereVectors;
+
+	for (float i = 0; i < glm::radians(360.0f); i += step) {
+		for (float j = 0; j < glm::radians(360.0f); j += step) {
+			//Triangle 1
+			sphereVectors.push_back(getPolarCoordinate(i, j));
+			sphereVectors.push_back(getPolarCoordinate(i + step, j));
+			sphereVectors.push_back(getPolarCoordinate(i + step, j + step));
+			//Triangle 2
+			sphereVectors.push_back(getPolarCoordinate(i + step, j + step));
+			sphereVectors.push_back(getPolarCoordinate(i, j + step));
+			sphereVectors.push_back(getPolarCoordinate(i, j));
+		}
+	}
+	return getNormal(sphereVectors);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+
+	//Close
+	if ((key == GLFW_KEY_ESCAPE || key == GLFW_KEY_Q) && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+}
 
 int main(){
-	printf("If you see this, the setup probably works. :)");
-	scanf("Hello");
+	glfwSetErrorCallback(error_callback);
+	glfwInit();
+
+	GLFWwindow* window;
+	window = glfwCreateWindow(1920 / 2, 1080 / 2, "Hello", NULL, NULL);
+	if (!window) {
+		printf("Failed to open window");
+		glfwTerminate();
+		return 0;
+	}
+	glfwMakeContextCurrent(window);
+
+	glfwSetKeyCallback(window, key_callback);
+	glewInit();
+
+	program = LoadShader("shader.vert", "shader.frag");
+
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	//handleInit();
+	//init();
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+	glEnable(GL_TEXTURE_2D);
+
+	glEnable(GL_CULL_FACE);
+
+	while (!glfwWindowShouldClose(window)) {
+		glUseProgram(program);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		//draw();
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
 	return 0;
 }
