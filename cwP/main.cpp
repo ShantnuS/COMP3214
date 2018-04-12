9772511d6013c77718070152614ee4da2326ece4
ShantnuS <shantnu@outlook.com>
2018-04-12 18:34:19 +0100

    cwp vs studio files

---
+++ cwP/main.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+// OpenGL (GLFW/GLEW)
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+// GLM (basics)
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// Bullet Physics (basics)
+#include <btBulletDynamicsCommon.h>
+
+// tiny_obj_loader
+#define TINYOBJLOADER_IMPLEMENTATION // only define in one file
+#include <tiny_obj_loader.h>
+
+// stb_image
+#define STB_IMAGE_IMPLEMENTATION // only define in one file
+#include <stb_image.h>
+
+
+
+int main()
+{
+	printf("If you see this, the setup probably works. :)");
+	return 0;
+}
